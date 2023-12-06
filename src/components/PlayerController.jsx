bb79bc1d0d9ce42fd9caa95fc907303605f7f73f
J_ Keyy <ashnazg1212@gmail.com>
2023-12-06 02:46:34 +0000

    feat:camera follow player

--- src/components/PlayerController.jsx
+++ src/components/PlayerController.jsx
@@ -3,20 +3,19 @@ import { CapsuleCollider, RigidBody } from "@react-three/rapier";
 import { useEffect, useRef } from "react";
 import Player from "./Player";
 import { useFrame, useThree } from "@react-three/fiber";
-import { mouseMovement } from "../lib/mouseMovement";
+import { Vector3 } from "three";
 
 export default function PlayerController() {
   console.log("rendered");
   const playerBody = useRef();
   const characterBody = useRef();
   const cameraRef = useRef();
-  const three = useThree();
+  // const three = useThree();
 
   const [subscribeKeys, getKeys] = useKeyboardControls();
   // const mouse = mouseMovement();
   useEffect(() => {
-    console.log(cameraRef.current);
-
+    // console.log(playerBody.current);
     const unsubcribeJump = subscribeKeys(
       (state) => state.jump,
       (pressed) => {
@@ -33,9 +32,12 @@ export default function PlayerController() {
       (state) => state.forward,
       (pressed) => {
         if (pressed) {
+          playerBody.current.setLinvel({ x: 2.0, y: 0.0, z: 0.0 }, true);
+          // console.log(playerBody.current.linvel());
         }
 
         if (!pressed) {
+          playerBody.current.linvel(0.0, 0.0, 0.0);
         }
       },
     );
@@ -87,8 +89,15 @@ export default function PlayerController() {
   }, []);
 
   // const mouse = mouseMovement();
+  const cameraOffset = new Vector3(0, 3, 5);
   useFrame((state, delta) => {
-    console.log(cameraRef.current.getSpherical());
+    if (!playerBody.current || !characterBody.current) return;
+    // const look = new Vector3().setFromSpherical(
+    //   cameraRef.current.getSpherical(),
+    // );
+    const position = playerBody.current.translation();
+    const newposition = position.add(cameraOffset);
+    cameraRef.current.setPosition(newposition, true);
   });
   return (
     <RigidBody
@@ -97,12 +106,12 @@ export default function PlayerController() {
       restitution={0.2}
       friction={2}
       position={[0, 5, 0]}
-      // enabledRotations={[false, true, false]}
-      // linearDamping={0.5}
-      // angularDamping={0.5}
+      enabledRotations={[false, true, false]}
+      linearDamping={0.5}
+      angularDamping={0.5}
     >
       <CameraControls ref={cameraRef} />
-      <Player position={[0, -0.8, 0]} />
+      <Player ref={characterBody} position={[0, -0.8, 0]} />
       <CapsuleCollider args={[0.5, 0.4]} translation={[0, 5, 0]} />
     </RigidBody>
   );
